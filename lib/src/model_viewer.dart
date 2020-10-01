5161eba27e0f0619b2c48abc4453ce7d2752833b
Oleg Korotin <o9818154818@yandex.ru>
2020-10-01 14:40:52 +0300

    Update model_viewer.dart

--- lib/src/model_viewer.dart
+++ lib/src/model_viewer.dart
@@ -137,7 +137,7 @@ class _ModelViewerState extends State<ModelViewer> {
         appStates.url.value = url;
         print('>>>> ModelViewer initializing... <$url>'); // DEBUG
         await webViewController.loadUrl(url);
-        await webViewController.scrollTo(10, 10);
+        await webViewController.scrollTo(100, 100);
       },
       navigationDelegate: (final NavigationRequest navigation) async {
         //print('>>>> ModelViewer wants to load: <${navigation.url}>'); // DEBUG
