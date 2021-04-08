fc5e6906c54e7a14d853c717b905b6044ae6b4c2
Oleg Korotin <o9818154818@yandex.ru>
2021-04-08 13:34:07 +0300

    .

--- lib/src/html_builder.dart
+++ lib/src/html_builder.dart
@@ -27,7 +27,7 @@ abstract class HTMLBuilder {
     html.write(
         '<body style="${gradient != null ? 'background: linear-gradient(180deg, rgb(${gradient[0].red}, ${gradient[0].green}, ${gradient[0].blue}) 0%, rgb(${gradient[1].red}, ${gradient[1].green}, ${gradient[1].blue}) 100%);' : 'background-color: rgb(${backgroundColor.red}, ${backgroundColor.green}, ${backgroundColor.blue})'}"><model-viewer');
     html.write(
-        ' src="${htmlEscape.convert(src)}" animation-name="$animationName"');
+        'id="mainModel" src="${htmlEscape.convert(src)}" animation-name="$animationName"');
     html.write(
         ' style="background-color: transparent; margin-top: 29%; margin-left: -10%; width: 100%; height: 100%; --poster-color: transparent; --progress-bar-height: 0px"');
     if (alt != null) {
@@ -81,7 +81,7 @@ abstract class HTMLBuilder {
     // TODO: shadow-softness
     html.writeln('></model-viewer>');
     html.writeln(
-        '<script>(() => {const modelViewer = document.querySelector("#paused-change-demo");self.setInterval(() => {modelViewer.animationName = modelViewer.animationName === "Armature.002|mixamo.com|Layer0" ?"Armature.006|mixamo.com|Layer0" : "Armature.002|mixamo.com|Layer0";}, 1500.0);})();</script>');
+        '<script>(() => {const modelViewer = document.querySelector("#mainModel");self.setInterval(() => {modelViewer.animationName = modelViewer.animationName === "Armature.002|mixamo.com|Layer0" ? "Armature.006|mixamo.com|Layer0" : "Armature.002|mixamo.com|Layer0";}, 1500.0);})();</script>');
     html.writeln('</body>');
     return html.toString();
   }
