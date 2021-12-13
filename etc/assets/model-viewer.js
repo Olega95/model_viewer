38cf6572a9f76192c5c6b4384049a96ce80be9eb
Oleg Korotin <o9818154818@yandex.ru>
2021-12-13 16:01:01 +0300

    Add files via upload

--- etc/assets/model-viewer.js
+++ etc/assets/model-viewer.js
@@ -53132,7 +53132,7 @@ var GLTFLoader = ( function () {
      var lightDef = this.lightDefs[ lightIndex ];
      var lightNode;
 
-     var color = new Color( 0xffffff );
+     var color = new Color( 0x000000 );
      if ( lightDef.color !== undefined ) color.fromArray( lightDef.color );
 
      var range = lightDef.range !== undefined ? lightDef.range : 0;
